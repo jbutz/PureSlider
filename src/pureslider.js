f0ed759242f606167cfb9e3c648df52eae149a59
Jason Butz <jason.butz@inin.com>
2017-02-03 11:30:12 -0500

    Fix CSS And Format JS

--- src/pureslider.js
+++ src/pureslider.js
@@ -3,29 +3,30 @@ class PureSlider {
     duration = 2000,
     actionMode = 'fade',
     element = null
-  } = {}){
+  } = {}) {
     this.options = {
       duration,
       actionMode,
       element
     }
-    this.el = element || document.querySelector('#pure-slider')
-    this.activeIndex = 0
-    this.el.children[this.activeIndex].classList.add('active')
+    this.el = element || document.querySelector('#pure-slider');
+    this.activeIndex = 0;
+    this.el.classList.add('pure-slider');
+    this.el.children[this.activeIndex].classList.add('active');
   }
 
-  slide(){
+  slide() {
     const actionMode = {
       // All action modes (how it works, what it will do) should be defined in this object
       fade: () => {
         this.el.classList.add('fade')
-        setInterval(()=>{
+        setInterval(() => {
           this.el.children[this.activeIndex].classList.remove('active')
           if (this.activeIndex === this.el.children.length - 1) {
             this.activeIndex = 0
             this.el.children[this.activeIndex].classList.add('active')
           } else {
-            this.el.children[this.activeIndex+1].classList.add('active')
+            this.el.children[this.activeIndex + 1].classList.add('active')
             this.activeIndex = this.activeIndex + 1
           }
         }, this.options.duration)
